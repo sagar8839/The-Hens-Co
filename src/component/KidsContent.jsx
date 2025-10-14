aa82bd344c16cc6ee6887eea0986a50e652c4c24
sagar8839 <sagargupta7067@gmail.com>
2025-10-14 11:40:38 +0530

    home section

--- src/component/KidsContent.jsx
+++ src/component/KidsContent.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+
 import styles from "./womenContent.module.css";
 import eggBox from "/images/kids.png";
 import yolkDish from "/images/yolks.webp";
--- src/component/SmallEggBoxContent.jsx
+++ src/component/SmallEggBoxContent.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+ 
 import styles from "./womenContent.module.css";
 import eggBox from "/images/6box.png";
 import yolkDish from "/images/yolks.webp";
