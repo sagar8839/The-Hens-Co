88912542dcc69218e750f1d36dc5f515f97a5a3f
sagar8839 <sagargupta7067@gmail.com>
2025-10-14 11:35:58 +0530

    Fix import path and case sensitivity for Vercel build

--- src/component/EggContent.jsx
+++ src/component/EggContent.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./womenContent.module.css";
+
 import eggBox from "/images/thirty.png";
 import yolkDish from "/images/yolks.webp";
 import brownBlueEggs from "/images/vbhen.png";
